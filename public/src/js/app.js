1f6bbc9ba67a9948c32823f0c300457aa17320b3
Dennis Burger <dennis@dutchwebworks.nl>
2018-02-04 12:49:48 +0100

    Added some tests and waiting for registration of service worker.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,9 @@
 if("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("/sw.js");
+	navigator.serviceWorker
+		.register("/sw.js")
+		.then(function(){
+			console.log("Service worker registered");
+		});
 } else {
 	document.getElementById("js-no-service-worker").removeAttribute("hidden");
 }
